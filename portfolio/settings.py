8fe4fa2f572ca83f4e2fd7d85481a62e108871af
Arnold Patten <apatten001@yahoo.com>
2019-03-07 11:44:36 -0500

    remove settings for debug urls

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -144,8 +144,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "credentials/static")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'credentials/media')
-MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
